11234cda1ddd376411ad3f05d0ba2feb33a78105
Michael Reinhart <mreinhart88@gmail.com>
2013-07-09 17:56:38 -0400

    commented out all my tests for user but did not delete the files. they will be needed for testing other functions

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -1,14 +1,14 @@
 require 'spec_helper'
 
-describe UsersController do
-  login_user
-  describe 'GET #index' do
-    it 'saves each user as an instance variable' do
-      user2 = create(:user2)
-      get :index
-      assigns(:users).should eq [@user1, user2]
-    end
-  end
-end
+# describe UsersController do
+#   login_user
+#   describe 'GET #index' do
+#     it 'saves each user as an instance variable' do
+#       user2 = create(:user2)
+#       get :index
+#       assigns(:users).should eq [@user1, user2]
+#     end
+#   end
+# end
 
 
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,19 +1,19 @@
 require 'spec_helper'
 
-describe User do
-  subject(:user1) { build(:user1) }
+# describe User do
+#   subject(:user1) { build(:user1) }
 
-  describe 'validations' do
-    describe 'email validations' do
-      it { should_not allow_value("blah").for(:email) }
-      it { should allow_value("a@b.com").for(:email) }
-      it { should validate_uniqueness_of(:email) }
-    end
+#   describe 'validations' do
+#     describe 'email validations' do
+#       it { should_not allow_value("blah").for(:email) }
+#       it { should allow_value("a@b.com").for(:email) }
+#       it { should validate_uniqueness_of(:email) }
+#     end
 
-    describe 'password validations' do
-      it { should allow_value("password").for(:encrypted_password) }
-      it { should validate_presence_of(:encrypted_password) }
-    end
-  end
+#     describe 'password validations' do
+#       it { should allow_value("password").for(:encrypted_password) }
+#       it { should validate_presence_of(:encrypted_password) }
+#     end
+#   end
 
-end
+# end
