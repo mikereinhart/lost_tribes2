1a481341ac5c07fd3c8b8f719b34986714b816b9
Michael Reinhart <mreinhart88@gmail.com>
2013-07-09 17:59:25 -0400

    added zip code to user thru migration. not tested yet

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130709203440) do
+ActiveRecord::Schema.define(:version => 20130709215758) do
 
   create_table "users", :force => true do |t|
     t.string   "email",                  :default => "", :null => false
@@ -27,6 +27,7 @@ ActiveRecord::Schema.define(:version => 20130709203440) do
     t.datetime "created_at",                             :null => false
     t.datetime "updated_at",                             :null => false
     t.string   "name"
+    t.integer  "zip"
   end
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
