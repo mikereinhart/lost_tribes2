d269a8e77297d118e60066b4b9aa47c774af9539
Ryan Kuhel <ryan.kuhel@gmail.com>
2013-07-09 22:15:22 -0400

    TDD index and new for users controller complete

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,7 +4,34 @@ class UsersController < ApplicationController
     @users = User.all
   end
 
-  # def new
-  #   @user = User.new
-  # end
+  def create 
+    @user = User.new(params[:user])
+    @user.save!
+
+    redirect_to '/'
+  end
+
+  def new
+    @user = User.new  
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params['id'])
+    if user.update_attributes(params['user'])
+      redirect_to users_path
+    else 
+      redirect_to vendors_path 
+    end
+  end
+
+  def destroy 
+    User.find(params[:id]).delete 
+    redirect_to users_path
+  end
+
+
 end
--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -1,14 +1,54 @@
 require 'spec_helper'
 
-# describe UsersController do
-#   login_user
-#   describe 'GET #index' do
-#     it 'saves each user as an instance variable' do
-#       user2 = create(:user2)
-#       get :index
-#       assigns(:users).should eq [@user1, user2]
-#     end
-#   end
-# end
+describe UsersController do 
+  subject {UsersController}
+  it {should < ApplicationController}
 
 
+  describe 'collection' do 
+    describe 'GET #index' do
+      it 'assigns all users to an instance variable' do
+        user = create(:user1)
+        user2 = create(:vendor)
+        get :index
+        assigns(:users).should eq [user, user2]
+      end
+    end
+
+    it 'renders the :index view' do
+      get :index
+      response.should render_template :index
+    end
+  end
+
+  describe 'Get #new' do
+    it 'assigns a new user to an instance variable' do 
+      get :new 
+      assigns(:user).should be_an_instance_of(User)
+      assigns(:user).should be_new_record
+    end
+    it 'render the :new view' do
+      get :new
+      response.should render_template :new
+    end
+  end
+
+  describe 'Post #create' do
+    context 'with valid attributes' do
+
+      it 'creates a new user' do
+        expect {
+          post :create, user: attributes_for(:user1)
+        }.to change{User.count}.by(1)
+      end
+
+    end
+  end
+
+
+
+
+
+
+  
+end
