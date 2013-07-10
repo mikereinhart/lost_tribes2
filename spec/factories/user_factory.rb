411f4a6160158695bc775d5edfcfabe7f23f08f6
Ryan Kuhel <ryan.kuhel@gmail.com>
2013-07-09 20:19:38 -0400

    vendor tests and model finished

--- spec/factories/user_factory.rb
+++ spec/factories/user_factory.rb
@@ -8,11 +8,16 @@ FactoryGirl.define do
     password_confirmation "password"
   end
 
-  factory :user2, class: User do
+  factory :vendor, class: User do
     # id 2
     name Faker::Name.name
     email Faker::Internet.email
+    vendor true 
     zip 44122
+    street_address "2333 norwood rd"
+    city "beachwood"
+    state 'Ohio'
+    phone_number "215 666 7777"
     password "wordpass"
     password_confirmation "wordpass"
   end
