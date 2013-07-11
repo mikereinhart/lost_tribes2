ccda0f736e00c7c5939d6af6c4fa937bcd842e14
Michael Reinhart <mreinhart88@gmail.com>
2013-07-10 20:12:23 -0400

    got to a certain point with event delete on the events/new page, but need controller to continue

---
+++ app/assets/javascripts/events.js
@@ -0,0 +1,29 @@
+
+function delete_event(e){
+  e.preventDefault();
+  console.log('Entered function delete_task');
+
+  var event_row = $(this).parent().parent();
+  var id_of_event = event_row.data('event_id');
+
+  $.ajax({
+    type: 'DELETE',
+    url: id_of_event,
+    success: function(data){
+      console.log('Success!');
+    }
+  });
+  return $(this);
+}
+
+
+
+
+
+
+
+$(function(){
+
+  $('tbody').on('click', '.delete-button', delete_event);
+
+});
