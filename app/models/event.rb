ceac69bd70701a290c4d413e9a9502f5da0653af
Chris Eichler <chriseichler@gmail.com>
2013-07-12 18:30:09 -0400

    updating controllers of this branch

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,5 +1,15 @@
 class Event < ActiveRecord::Base
-  attr_accessible :city, :date, :description, :state, :street_address, :street_address2, :title, :zip_code, :created_at, :updated_at
 
+  attr_accessible :city, :date, :description, :state, :street_address, :street_address2, :title, :zip_code, :creator_id, :updated_at, :created_at
 
+  belongs_to :creator, class_name: "User", inverse_of: :created_events 
+  has_and_belongs_to_many :users
+
+  validates :city, presence: true
+  validates :date, presence: true
+  validates :description, presence: true
+  validates :state, presence: true
+  validates :street_address, presence: true
+  validates :title, presence: true
+  validates :zip_code, presence: true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,36 @@
 class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
-  # :token_authenticatable, :confirmable,
+  # :token_authenticatable,
   # :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable,
+  devise :database_authenticatable, :confirmable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
   # Setup accessible (or protected) attributes for your model
-  attr_accessible :email, :password, :password_confirmation, :remember_me, :name
-  # attr_accessible :title, :body
+  attr_accessible :email, :password, :password_confirmation, :remember_me, :name, 
+  :zip, :street_address, :street_address2, :city, :state, :phone_number, :vendor, :admin, :street_address_2  # attr_accessible :title, :body
+
+  validates :zip, presence: true, length: { :is => 5 } 
+  validate :vendor_address # or validate? 
+
+  def vendor_address
+    if self.vendor? && self.street_address.nil?
+      self.errors.add(:street_address, 'Street Address is required for vendors.')
+    end
+
+    if self.vendor? && self.city.nil?
+      self.errors.add(:city, 'City is a required field for vendors.')
+    end 
+
+    if self.vendor? && self.state.nil?
+      self.errors.add(:state, 'State is a required field.')
+    end
+
+    if self.vendor? && self.phone_number.nil?
+      self.errors.add(:phone_number, "Phone number is a required field.")
+    end
+  end
+
+  has_and_belongs_to_many :events
+  has_many :created_events, foreign_key: 'creator_id', class_name: "Event", inverse_of: :creator
+
 end
