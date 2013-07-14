aa4c8dc1f87ea129f4825d92e81c6061a2a03034
Chris Eichler <chriseichler@gmail.com>
2013-07-14 13:49:07 -0400

    raked and seeded db w mike changes and fixed formatting of main erb

--- db/schema.rb
+++ db/schema.rb
@@ -21,7 +21,6 @@ ActiveRecord::Schema.define(:version => 20130714164405) do
     t.string   "city"
     t.string   "state"
     t.string   "zip_code"
-    t.integer  "creator_id"
     t.datetime "created_at",      :null => false
     t.datetime "updated_at",      :null => false
     t.text     "description"
