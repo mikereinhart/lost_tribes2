5025313f8c903fcaba305fbbc228307fc69ce15b
Ryan Kuhel <ryan.kuhel@gmail.com>
2013-07-13 21:18:21 -0400

    starting csv downloading for users and events

--- app/models/event.rb
+++ app/models/event.rb
@@ -12,4 +12,13 @@ class Event < ActiveRecord::Base
   validates :street_address, presence: true
   validates :title, presence: true
   validates :zip_code, presence: true
+
+  def self.to_csv(options = {})
+    CSV.generate(options) do |csv|
+      csv << column_names
+      all.each do |event|
+        csv << event.attributes.values_at(*column_names)
+      end
+    end
+  end
 end
